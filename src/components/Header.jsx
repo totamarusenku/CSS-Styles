fea71a487b458b03cf4afaf8c53b2e15b9141d18
The Daijin <6650280+totamarusenku@users.noreply.github.com>
2025-11-15 22:56:34 +0530

    Introduces Tailwindcss

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import logo from "../assets/logo.png";
-import "./Header.css";
 
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+    <header className="flex flex-col items-center mt-8 mb-16">
+      <img
+        src={logo}
+        alt="A canvas"
+        className="object-contain mb-8 w-44 h-44"
+      />
+      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800">
+        ReactArt
+      </h1>
       <p>A community of artists and art-lovers.</p>
     </header>
   );
